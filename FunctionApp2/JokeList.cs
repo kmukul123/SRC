974bce9e1aaf3caedc54302704ddeb47c7e17c34
Mukul Kumar <Mukul_Kumar@sunsuper.com.au>
2019-08-13 16:28:09 +1000

    Adding Azure function to showcase Simple Azure Function1
    This function is not static but is a sample of a function in a class which is instantiated everytime
    the class also uses dependency injection.

---
+++ FunctionApp2/JokeList.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionApp2
+{
+    public class JokesEntity : TableEntity
+    {
+    }
+
+
+}
---
+++ FunctionApp2/Startup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+[assembly: FunctionsStartup(typeof(FunctionApp2.StartUp))]
+
+namespace FunctionApp2
+{
+    public class StartUp : FunctionsStartup
+    {
+
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            //builder.Services.
+            //builder.Services.AddHttpClient();
+            
+            builder.Services.AddSingleton<ICache, Cache>();
+        }
+    }
+
+
+}

350d8bb427c61595399660a2c3ac674401af5c48
Mukul Kumar <Mukul.Kumar@brisbane.qld.gov.au>
2026-04-15 19:43:17 +1000

    fixing sign.cmd

--- OutlookRemindersOntop/OutlookRemindersOntop/sign.cmd
+++ OutlookRemindersOntop/OutlookRemindersOntop/sign.cmd
@@ -1,11 +1,41 @@
+@echo off
 setlocal
-set path=%Path%;C:\Program Files (x86)\Microsoft SDKs\ClickOnce\SignTool
+set "SIGNE_PATH=C:\Program Files (x86)\Microsoft SDKs\ClickOnce\SignTool\signtool.exe"
+set "CERT_FILE=%~dp0\TestKey.pfx"
+set "TARGET_EXE=%~dp0\bin\OutlookRemindersOntop.Release\RemindersOntop.exe"
 
-pushd %~dp0\bin\OutlookRemindersOntop.Release
+if not exist "%SIGNE_PATH%" (
+    echo Error: signtool.exe not found at %SIGNE_PATH%
+    exit /b 1
+)
 
-signtool sign RemindersOntop.exe
-signtool timestamp /t http://timestamp.digicert.com RemindersOntop.exe
+if not exist "%CERT_FILE%" (
+    echo Error: Certificate file %CERT_FILE% not found.
+    exit /b 1
+)
 
+if not exist "%TARGET_EXE%" (
+    echo Error: Target executable %TARGET_EXE% not found. Please build the project first.
+    exit /b 1
+)
 
+set /p PASSWORD="Enter PFX password: "
 
+echo Signing %TARGET_EXE%...
+"%SIGNE_PATH%" sign /f "%CERT_FILE%" /p "%PASSWORD%" /v "%TARGET_EXE%"
+if %errorlevel% neq 0 (
+    echo Error signing file.
+    exit /b %errorlevel%
+)
+
+echo Adding timestamp...
+"%SIGNE_PATH%" timestamp /t http://timestamp.digicert.com "%TARGET_EXE%"
+if %errorlevel% neq 0 (
+    echo Error adding timestamp.
+    exit /b %errorlevel%
+)
+
+echo.
+echo Success! RemindersOntop.exe has been signed and timestamped.
+pause
 endlocal
